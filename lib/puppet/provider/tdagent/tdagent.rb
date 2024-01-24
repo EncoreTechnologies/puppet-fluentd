441db8e55f2c369b2afbeefdded75863d475dd54
Alex Chrystal <alex.chrystal@encore.tech>
2024-01-24 09:45:58 -0500

    testing

--- lib/puppet/provider/tdagent/tdagent.rb
+++ lib/puppet/provider/tdagent/tdagent.rb
@@ -9,7 +9,7 @@ Puppet::Type.type(:tdagent).provide(:tdagent, parent: Puppet::Provider::Package)
     raise Puppet::Error, "Provider #{name} package command '#{command}' does not exist on this host, it couldn't be found in the following paths: #{paths}"
   end
 
-  def self.provider_command
+  def self.provider_command(repo_version)
     if Puppet::Util::Platform.windows?
       tdagent_cmd = 'fluent-gem.bat'
       search_paths = [
@@ -17,7 +17,7 @@ Puppet::Type.type(:tdagent).provide(:tdagent, parent: Puppet::Provider::Package)
         'C:\\opt\\td-agent\\embedded\\bin',
       ]
     else
-      tdagent_cmd = tdagent_cmd_for_linux
+      tdagent_cmd = tdagent_cmd_for_linux(repo_version)
       search_paths = [
         '/usr/sbin',
         '/opt/td-agent/usr/sbin',
@@ -25,37 +25,32 @@ Puppet::Type.type(:tdagent).provide(:tdagent, parent: Puppet::Provider::Package)
     end
     which_from_paths(tdagent_cmd, search_paths)
   end
-
-  def self.tdagent_cmd_for_linux
-    version = Facter.value(:repo_version)
-    case version
+  
+  def self.tdagent_cmd_for_linux(repo_version)
+    case repo_version
     when '4'
       'td-agent-gem'
     else
       'fluent-gem'
     end
   end
-
+  
   def create
-    system(self.class.provider_command, 'gem', 'install', resource[:name], '-v', resource[:ensure], '--source', resource[:source], *resource[:install_options])
+    system(self.class.provider_command(resource[:repo_version]), 'gem', 'install', resource[:name], '-v', resource[:ensure], '--source', resource[:source], *resource[:install_options])
   end
-
+  
   def destroy
-    system(self.class.provider_command, 'gem', 'uninstall', resource[:name])
+    system(self.class.provider_command(resource[:repo_version]), 'gem', 'uninstall', resource[:name])
   end
-
+  
   def exists?
-    output = `#{self.class.provider_command} gem list --local`
+    output = `#{self.class.provider_command(resource[:repo_version])} gem list --local`
     output.include?(resource[:name])
   end
-
+  
   def version
-    output = `#{self.class.provider_command} gem list --local`
+    output = `#{self.class.provider_command(resource[:repo_version])} gem list --local`
     match = output.match(/#{Regexp.escape(resource[:name])}\s+\((\S+)\)/)
     match[1] if match
-  end
-
-  def version=(value)
-    system(self.class.provider_command, 'gem', 'update', resource[:name], '-v', value, '--source', resource[:source], *resource[:install_options])
   end
 end
