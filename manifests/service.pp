bb3da2fc911f9910c32fe6565f57180d7c5067bf
Alex Chrystal <alex.chrystal@encore.tech>
2024-02-07 15:05:23 -0500

    fixing service to use name based on os

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,19 +1,14 @@
 # Resource for managing the FluentD service
+# Resource for managing the FluentD service
 class fluentd::service inherits fluentd {
   if $fluentd::service_manage {
-    # On Windows
-    # Fluentd v3 and older required us to self-register the Windows services
-    # In Fluentd v4+, the Windows service is registered as part of the MSI installer
-    if ($facts['os']['family'] == 'windows' and versioncmp($fluentd::repo_version, '3') <= 0) {
-      fluentd_windows_service { $fluentd::service_name_windows:
-        ensure    => present,
-        command   => 'C:/opt/td-agent/embedded/bin/fluentd.bat',
-        subscribe => Package[$fluentd::package_name],
-        notify    => Service[$fluentd::service_name_windows],
-      }
+    # Determine the service name based on the OS family and Fluentd version
+    $service_name = $facts['os']['family'] ? {
+      'windows' => $fluentd::service_name_windows,
+      default   => $fluentd::service_name,
     }
 
-    service { $fluentd::service_name:
+    service { $service_name:
       ensure => $fluentd::service_ensure,
       enable => $fluentd::service_enable,
     }
