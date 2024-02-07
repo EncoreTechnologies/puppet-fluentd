e730482819fdec77049cd421de69967fd09b2e34
Alex Chrystal <alex.chrystal@encore.tech>
2024-02-07 14:39:16 -0500

    adding search paths to provider

--- lib/puppet/provider/package/tdagent.rb
+++ lib/puppet/provider/package/tdagent.rb
@@ -11,8 +11,29 @@ Puppet::Type.type(:package).provide :tdagent, :parent => :gem do
 
   has_features :install_options
 
+  def self.which_from_paths(command, paths)
+    paths.each do |dir|
+      dest = File.expand_path(File.join(dir, command))
+      return dest if FileTest.file?(dest) && FileTest.executable?(dest)
+    end
+    raise Puppet::Error, "Provider #{name} package command '#{command}' does not exist on this host, it couldn't be found in the following paths: #{paths}"
+  end
+
   def self.provider_command
-    command(:gemcmd)
+    if Puppet::Util::Platform.windows?
+      gem_cmd = 'fluent-gem.bat'
+      search_paths = [
+        'C:\\opt\\td-agent\\bin',
+        'C:\\opt\\td-agent\\embedded\\bin',
+      ]
+    else
+      gem_cmd = 'td-agent-gem'
+      search_paths = [
+        '/usr/sbin',
+        '/opt/td-agent/usr/sbin',
+      ]
+    end
+    which_from_paths(gem_cmd, search_paths)
   end
 end
 
@@ -26,7 +47,28 @@ Puppet::Type.type(:package).provide :fluentd, :parent => :gem do
 
   has_features :install_options
 
+  def self.which_from_paths(command, paths)
+    paths.each do |dir|
+      dest = File.expand_path(File.join(dir, command))
+      return dest if FileTest.file?(dest) && FileTest.executable?(dest)
+    end
+    raise Puppet::Error, "Provider #{name} package command '#{command}' does not exist on this host, it couldn't be found in the following paths: #{paths}"
+  end
+
   def self.provider_command
-    command(:gemcmd)
+    if Puppet::Util::Platform.windows?
+      gem_cmd = 'fluent-gem.bat'
+      search_paths = [
+        'C:\\opt\\td-agent\\bin',
+        'C:\\opt\\td-agent\\embedded\\bin',
+      ]
+    else
+      gem_cmd = 'fluent-gem'
+      search_paths = [
+        '/usr/sbin',
+        '/opt/td-agent/usr/sbin',
+      ]
+    end
+    which_from_paths(gem_cmd, search_paths)
   end
 end
