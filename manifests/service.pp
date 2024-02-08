a66d6377f7cd02c1dc426aa5e00294495dcced40
Alex Chrystal <alex.chrystal@encore.tech>
2024-02-08 14:29:08 -0500

    fixing var name to avoid conflicts with module param

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,12 +3,12 @@ class fluentd::service {
   include fluentd
   if $fluentd::service_manage {
     # Determine the service name based on the OS family and Fluentd version
-    $service_name = $facts['os']['family'] ? {
+    $_service_name = $facts['os']['family'] ? {
       'windows' => $fluentd::service_name_windows,
       default   => $fluentd::service_name,
     }
 
-    service { $service_name:
+    service { $_service_name:
       ensure => $fluentd::service_ensure,
       enable => $fluentd::service_enable,
     }
